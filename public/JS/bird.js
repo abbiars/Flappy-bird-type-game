ebc54fa6ca6ad099c8bc0cffdb238ef21b71cd10
Abbi <abdallaharson@gmail.com>
2025-11-18 11:19:16 +0100

    Lo til  las vegas bakgrunn og lagte område for pipes
    lag pipe.js neste med bitchasspipe bilden

--- public/JS/bird.js
+++ public/JS/bird.js
@@ -1,7 +1,7 @@
-let birdY = 0;
-let birdVelocity = 0;
-const gravity = 0.6;
-const jumpPower = -12;
+let birdY = 0; // den vertikale posisjonen til birden
+let birdVelocity = 0; // hastigheten til birden oppover eller nedover
+const gravity = 0.6; // en verdi som bestemmer hvor raskt birden faller nedover
+const jumpPower = -12; // en verdi som bestemmer hvor høyt birden hopper oppover
 
 // Legger til funksjon fall for gravitasjon. Birden faller nedover ved å øke birdY med gravity-verdien og oppdatere transform-stilen til birdImage for å flytte den nedover på skjermen.
 function fall() {
@@ -12,10 +12,9 @@ function fall() {
 
 setInterval(() => {
   fall()
-}, 1000 / 60); // Kaller fall-funksjonen 60 ganger per sekund for jevn bevegelse.
+}, 1000 / 60); // Kaller fall-funksjonen 60 ganger per sekund for jevn bevegelse.. Litt unødvendig men det går bra.
 
 // Legger til funksjon jump for å få birden til å hoppe oppover ved å redusere birdY med 50 piksler og oppdatere transform-stilen til birdImage for å flytte den oppover på skjermen.
-
 function jump() {
   console.log('it workie!')
   birdVelocity = jumpPower;
@@ -23,24 +22,26 @@ function jump() {
 
 
 const birdImage = new Image();
-birdImage.src = './assets/bird/bitchassfugl.png';
-birdImage.width = 69;
-birdImage.height = 69;
-birdImage.style.position = 'absolute';
-birdImage.style.left = '240px';
-birdImage.style.top = '100px';
+birdImage.src = './assets/bird/bitchassfugl.png'; // Hvor birdy bildet ligger
+birdImage.width = 69; //bredden til fuglen
+birdImage.height = 69; // høyden til fuglen
+birdImage.style.position = 'absolute'; // gjør at vi kan plassere birden hvor som helst på skjermen
+birdImage.style.left = '240px'; // Hvor langt FRA venstre birden skal være
+birdImage.style.top = '100px'; // Hvor langt fra toppen birden skal være
 document.body.appendChild(birdImage);
 
-document.onclick = () => {
-  jump()
-}
 
+// dette gjør spnn at når du presser spacebar eller klikker med musa så gjør fuglen jump funksjonen
 document.onkeydown = (event) => {
-  if (event.code === 'Space' || event.key === ' ') {
+  if (event.code === 'Space' || event.key === ' ') { // funker ikke uten 3 = tegn. Vetsje koffor
     event.preventDefault()
     jump()
   }
 }
+document.onclick = () => {
+  jump()
+}
+
 
 let all; "fisk"
 
--- public/JS/main.js
+++ public/JS/main.js
@@ -2,16 +2,8 @@ export function jump() {
   console.log('it workie')
 }
 
-document.onclick = () => {
-  jump()
-}
 
-document.onkeydown = (event) => {
-  if (event.code === 'Space' || event.key === ' ') {
-    event.preventDefault()
-    jump()
-  }
-}
+
 
 const all = { jump }
 export default all;
